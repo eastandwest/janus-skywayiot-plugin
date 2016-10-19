4c2315cd49c1c901b886ab465f5fe987958a6fc5
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-19 05:02:42 +0000

    external interface implemented

--- plugins/janus_skywayiot.c
+++ plugins/janus_skywayiot.c
@@ -165,6 +165,17 @@ static janus_callbacks *gateway = NULL;
 static GThread *handler_thread;
 static GThread *watchdog;
 static void *janus_skywayiot_handler(void *data);
+static int create_skywayiot_extinterface(int dataport, char *mediaendpoint, int mediaport, char *listenaddr);
+
+static void *thread_receive_external_data(void *data);
+
+static void relay_extern_data(gpointer handle, gpointer session, gpointer data);
+
+typedef struct data_with_handleid {
+	guint64 handle_id;
+	char *data;
+	int data_len;
+} data_with_handleid;
 
 typedef struct janus_skywayiot_message {
  janus_plugin_session *handle;
@@ -194,6 +205,10 @@ typedef struct janus_skywayiot_session {
 static GHashTable *sessions;
 static GList *old_sessions;
 static janus_mutex sessions_mutex;
+int media_fd; /* socket for external media stream */
+int data_fd;  /* socket for external data stream */
+struct sockaddr_in g_media_remaddr;
+struct sockaddr_in g_data_remaddr;
 
 static void janus_skywayiot_message_free(janus_skywayiot_message *msg) {
  if(!msg || msg == &exit_message)
@@ -276,8 +291,38 @@ int janus_skywayiot_init(janus_callbacks *callback, const char *config_path) {
  g_snprintf(filename, 255, "%s/%s.cfg", config_path, JANUS_SKYWAYIOT_PACKAGE);
  JANUS_LOG(LOG_VERB, "Configuration file: %s\n", filename);
  janus_config *config = janus_config_parse(filename);
- if(config != NULL)
-  janus_config_print(config);
+
+ GList *cl = NULL;
+ if(config != NULL) {
+	 cl = janus_config_get_categories(config);
+ }
+
+ while(cl != NULL) {
+	 janus_config_category *cat = (janus_config_category *)cl->data;
+	 if(cat->name == NULL || strcasecmp(cat->name, "external-interface") != 0) {
+		 cl = cl->next;
+		 continue;
+	 }
+
+	 JANUS_LOG(LOG_INFO, "config:: name of category '%s'\n", cat->name);
+	 janus_config_item *dataport = janus_config_get_item(cat, "dataport");
+	 janus_config_item *mediaendpoint = janus_config_get_item(cat, "mediaendpoint");
+	 janus_config_item *mediaport = janus_config_get_item(cat, "mediaport");
+	 janus_config_item *listenaddr = janus_config_get_item(cat, "listenaddr");
+
+	 if(dataport == NULL || dataport->value == NULL
+			 || mediaendpoint == NULL || mediaendpoint->value == NULL
+			 || mediaport == NULL || mediaport->value == NULL
+			 || listenaddr == NULL || listenaddr->value == NULL) {
+		 JANUS_LOG(LOG_WARN, "  -- Invalid dataport, mediaport, listenaddr, we'll skip opening '%s'. \n", cat->name);
+		 cl = cl->next;
+		 continue;
+	 } else {
+		create_skywayiot_extinterface(atoi(dataport->value), (char *)mediaendpoint->value, atoi(mediaport->value), (char *)listenaddr->value);
+		cl = cl->next;
+	 }
+ }
+ janus_config_print(config);
  /* This plugin actually has nothing to configure... */
  janus_config_destroy(config);
  config = NULL;
@@ -515,6 +560,8 @@ void janus_skywayiot_setup_media(janus_plugin_session *handle) {
 void janus_skywayiot_incoming_rtp(janus_plugin_session *handle, int video, char *buf, int len) {
  if(handle == NULL || handle->stopped || g_atomic_int_get(&stopping) || !g_atomic_int_get(&initialized))
   return;
+
+
  /* Simple echo test */
  if(gateway) {
   /* Honour the audio/video active flags */
@@ -526,6 +573,20 @@ void janus_skywayiot_incoming_rtp(janus_plugin_session *handle, int video, char 
   if(session->destroyed)
    return;
   if((!video && session->audio_active) || (video && session->video_active)) {
+ 		char* ext_data;
+		int id_len = sizeof(guint64);
+		guint64 handle_id = (guint64)handle;
+		socklen_t addrlen = sizeof(g_media_remaddr);
+
+ 		ext_data = (char *)malloc( id_len + len );
+		memcpy(ext_data, &handle_id, id_len);
+		memcpy(ext_data + id_len, buf, len);
+
+		if((void *)&g_media_remaddr != NULL) {
+			sendto(media_fd, ext_data, (id_len + len), 0, (struct sockaddr *)&g_media_remaddr, addrlen);
+		}
+		g_free(ext_data);
+
    /* Save the frame if we're recording */
    janus_recorder_save_frame(video ? session->vrc : session->arc, buf, len);
    /* Send the frame back */
@@ -566,6 +627,22 @@ void janus_skywayiot_incoming_data(janus_plugin_session *handle, char *buf, int 
    return;
   if(buf == NULL || len <= 0)
    return;
+
+ 	char* ext_data;
+	int id_len = sizeof(guint64);
+	guint64 handle_id = (guint64)handle;
+	socklen_t addrlen = sizeof(g_data_remaddr);
+
+ 	ext_data = (char *)malloc( id_len + len );
+	memcpy(ext_data, &handle_id, id_len);
+	memcpy(ext_data + id_len, buf, len);
+
+	if((void *)&g_data_remaddr != NULL) {
+		sendto(data_fd, ext_data, (id_len + len), 0, (struct sockaddr *)&g_data_remaddr, addrlen);
+	}
+	g_free(ext_data);
+
+
   char *text = g_malloc0(len+1);
   memcpy(text, buf, len);
   *(text+len) = '\0';
@@ -988,3 +1065,97 @@ error:
  JANUS_LOG(LOG_VERB, "Leaving SkywayIoT handler thread\n");
  return NULL;
 }
+
+static int create_skywayiot_extinterface(int dataport, char *mediaendpoint, int mediaport, char *listenaddr) {
+	JANUS_LOG(LOG_INFO, "create external interface : dataport = %d, mediaendpoint + %s, mediaport = %d, listenaddr = %s\n", dataport, mediaendpoint, mediaport, listenaddr);
+
+	struct sockaddr_in data_sockaddr;      /* sockaddr for data channel */
+
+	/* create a UDP socket for ext interface of data channel*/
+	if ((data_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		JANUS_LOG(LOG_WARN, "cannot create socket for ext interface of data channel\n");
+		return -1;
+	}
+
+	/* bind the socket to any valid IP address and a specific port */
+	memset((char *)&data_sockaddr, 0, sizeof(data_sockaddr));
+	data_sockaddr.sin_family = AF_INET;
+	data_sockaddr.sin_addr.s_addr = htonl(inet_addr(listenaddr));
+	data_sockaddr.sin_port = htons(dataport);
+
+	if (bind(data_fd, (struct sockaddr *)&data_sockaddr, sizeof(data_sockaddr)) < 0) {
+		JANUS_LOG(LOG_WARN, "bind failed for ext interface of data channel\n");
+		return -1;
+	}
+
+	/* create a UDP socket for ext interface of media channel*/
+	if ( (media_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		JANUS_LOG(LOG_WARN, "cannot create socket for ext interface of media channel\n");
+		return -1;
+	}
+
+	/* set media endpoint address and port */
+
+	memset((char *)&g_media_remaddr, 0, sizeof(g_media_remaddr));
+	g_media_remaddr.sin_family = AF_INET;
+	g_media_remaddr.sin_addr.s_addr = htonl(inet_addr(mediaendpoint));
+	g_media_remaddr.sin_port = htons(mediaport);
+
+	JANUS_LOG(LOG_INFO, "succeed to create ext interface both data and media channel\n");
+
+	/* create thread to receive udp datagram for each channel */
+	GError *error = NULL;
+	g_thread_try_new("skywayiot_data_thread", &thread_receive_external_data, NULL, &error);
+	if(error != NULL) {
+		JANUS_LOG(LOG_WARN, "Got error %d (%s) while launching the data channel ext interface thread...\n", error->code, error->message ? error->message : "??");
+		return -1;
+	}
+	return 0;
+}
+
+static void *thread_receive_external_data(void *data /* to avoid warning */) {
+	char buff[1500];
+	int bytes_recv;
+
+	socklen_t addr_len = sizeof(g_data_remaddr);
+
+	guint64 handle_id;
+	int handle_id_len = sizeof(handle_id);
+	int data_len = 0;
+	data_with_handleid parsed = {
+		handle_id: 0,
+		data:      (char *) NULL,
+		data_len:  0
+	};
+
+
+
+	while(1 /* fixme: detect plugin termination */ ) {
+		bytes_recv = recvfrom(data_fd, buff,  1500, 0, (struct sockaddr *)&g_data_remaddr, &addr_len);
+
+		if(bytes_recv > handle_id_len) {
+			memcpy(&handle_id, buff, (size_t)handle_id_len);
+
+			data_len = bytes_recv - handle_id_len;
+
+			parsed.handle_id = handle_id;
+			parsed.data = buff + handle_id_len;
+			parsed.data_len = data_len;
+
+			g_hash_table_foreach(sessions, &relay_extern_data, &parsed);
+		}
+	}
+
+	return NULL;
+}
+
+static void relay_extern_data(gpointer handle, gpointer session, gpointer data) {
+	data_with_handleid *_data = (data_with_handleid *)data;
+
+	guint64 handle_id = (guint64)handle;
+
+	if(handle_id == _data->handle_id) {
+		gateway->relay_data(handle, (void *)_data->data, _data->data_len);
+	}
+}
+
